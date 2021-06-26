6ecd90d6e77a3a12e778013de50cf7f35d281bd0
fvictoriano <fillipe_victoriano@live.com>
2021-06-26 06:37:21 -0300

    search js

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,57 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+
+let estado = true;
+function abrirPesquisa() {
+    if(estado === true) {
+      document.getElementById('pesquisa1').style.width = "200px";
+    } else {
+      document.getElementById('pesquisa1').style.width = "0";
+    }
+    estado = !estado;
+
+    let valorPesquisa = document.getElementById('pesquisa1').value;
+
+    if (valorPesquisa === 'Banho' || valorPesquisa === 'banho' ) {
+      let elemento1 = document.getElementById('tosa');
+      elemento1.classList.add("d-none");
+      let elemento2 = document.getElementById('acessorios');
+      elemento2.classList.add("d-none");
+      let elemento3 = document.getElementById('alimentacao');
+      elemento3.classList.add("d-none");
+      document.getElementById('pesquisa1').value = '';
+    }
+
+    if (valorPesquisa === 'Tosa' || valorPesquisa === 'tosa' ) {
+      let elemento1 = document.getElementById('banho');
+      elemento1.classList.add("d-none");
+      let elemento2 = document.getElementById('acessorios');
+      elemento2.classList.add("d-none");
+      let elemento3 = document.getElementById('alimentacao');
+      elemento3.classList.add("d-none");
+      document.getElementById('pesquisa1').value = '';
+    }
+
+    if (valorPesquisa === 'Acessórios' || valorPesquisa === 'acessórios' ) {
+      let elemento1 = document.getElementById('banho');
+      elemento1.classList.add("d-none");
+      let elemento2 = document.getElementById('tosa');
+      elemento2.classList.add("d-none");
+      let elemento3 = document.getElementById('alimentacao');
+      elemento3.classList.add("d-none");
+      document.getElementById('pesquisa1').value = '';
+    }
+
+    if (valorPesquisa === 'Alimentação' || valorPesquisa === 'alimentação' ) {
+      let elemento1 = document.getElementById('banho');
+      elemento1.classList.add("d-none");
+      let elemento2 = document.getElementById('tosa');
+      elemento2.classList.add("d-none");
+      let elemento3 = document.getElementById('acessorios');
+      elemento3.classList.add("d-none");
+      document.getElementById('pesquisa1').value = '';
+    }
+
+}  
